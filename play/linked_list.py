353ecb9e07dd33f78b25670c4de29e765157a81a
AmrAboshaisha <90650510+AmrAboshaisha@users.noreply.github.com>
2022-10-06 19:11:21 +0100

    Implemented insert_first and delete_first

--- play/linked_list.py
+++ play/linked_list.py
@@ -1,4 +1,20 @@
 from linked_list_node import LinkedListNode as Node
 
 class LinkedList:
-    pass
+    def __init__(self, head=None):
+        self.head = head
+
+    def insert_first(self, x):
+        if self.head == None: self.head = x
+        else:
+            first_node = self.head
+            self.head = x
+            x.next = first_node
+
+    def delete_first(self):
+        if self.head == None: pass
+        else:
+            first_node = self.head
+            second_node = first_node.next
+            self.head = second_node
+            return first_node
--- play/test_linked_list.py
+++ play/test_linked_list.py
@@ -1,8 +1,41 @@
 import unittest
-from linked_list_node import LinkedListNode
+from linked_list_node import LinkedListNode as Node
 from linked_list import LinkedList
 
 class LinkedListTest(unittest.TestCase):
-    def test_linked_list_init(self):
-        pass
+    
+    def test_non_empty_linked_list(self):
+        first_node = Node(item="First Node")
+        llist = LinkedList(head=first_node)
+        self.assertEqual(llist.head, first_node)
 
+    def test_insert_first(self):
+        # Arrange
+        first_node = Node(item="First Node")
+        llist = LinkedList(head=first_node)
+        second_node = Node(item="Second Node")
+        # Act
+        llist.insert_first(second_node)
+        # Assert
+        self.assertEqual(llist.head, second_node)
+        self.assertEqual(second_node.next, first_node)
+
+    def test_delete_first(self):
+        # Arrange
+        first_node = Node(item="First Node")
+        llist = LinkedList(head=first_node)
+        # Act
+        llist.delete_first()
+        # Assert
+        self.assertEqual(llist.head, None)
+        
+        # Another case with 2 nodes
+        first_node = Node(item="First Node")
+        second_node = Node(item="Second Node")
+        first_node.next = second_node
+        llist = LinkedList(head=first_node)
+        llist.delete_first()
+        self.assertEqual(llist.head, second_node)
+
+if __name__ == "__main__":
+    unittest.main()
