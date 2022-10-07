45a489a6461bc144bab5bac3eae239e7c655a894
AmrAboshaisha <90650510+AmrAboshaisha@users.noreply.github.com>
2022-10-07 17:28:42 +0100

    Added methods: setUp, get_at, set_at and version 0 of insert_at

--- play/linked_list.py
+++ play/linked_list.py
@@ -4,6 +4,22 @@ class LinkedList:
     def __init__(self, head=None):
         self.head = head
 
+    def get_at(self, i):
+        current_node = self.head
+        next_node = current_node.next
+        
+        if current_node == None: return None
+        else:
+            idx = 0
+            while idx != i:
+                idx += 1
+                current_node = next_node
+                next_node = next_node.next
+            return current_node
+
+    def set_at(self, i, x):
+        self.get_at(i).item = x
+
     def insert_first(self, x):
         if self.head == None: self.head = x
         else:
@@ -18,3 +34,17 @@ class LinkedList:
             second_node = first_node.next
             self.head = second_node
             return first_node
+
+    def insert_at(self, i, x):
+        node_to_replace = self.get_at(i)
+        if node_to_replace == None:
+            self.head = x
+        else:
+            if i == 0:
+                self.head = x
+                x.next = node_to_replace
+            else:
+                previous_node = self.get_at(i-1)
+                previous_node.next = x
+                x.next = node_to_replace
+
--- play/test_linked_list.py
+++ play/test_linked_list.py
@@ -4,38 +4,51 @@ from linked_list import LinkedList
 
 class LinkedListTest(unittest.TestCase):
     
+    def setUp(self):
+        self.first_node = Node(item="First Node")
+        self.second_node = Node(item="Second Node")
+        self.llist = LinkedList(head=self.first_node)
+
     def test_non_empty_linked_list(self):
-        first_node = Node(item="First Node")
-        llist = LinkedList(head=first_node)
-        self.assertEqual(llist.head, first_node)
+        self.assertEqual(self.llist.head.item, "First Node")
 
     def test_insert_first(self):
-        # Arrange
-        first_node = Node(item="First Node")
-        llist = LinkedList(head=first_node)
-        second_node = Node(item="Second Node")
         # Act
-        llist.insert_first(second_node)
+        self.llist.insert_first(self.second_node)
         # Assert
-        self.assertEqual(llist.head, second_node)
-        self.assertEqual(second_node.next, first_node)
+        self.assertEqual(self.llist.head, self.second_node)
+        self.assertEqual(self.second_node.next, self.first_node)
 
     def test_delete_first(self):
-        # Arrange
-        first_node = Node(item="First Node")
-        llist = LinkedList(head=first_node)
         # Act
-        llist.delete_first()
+        self.llist.delete_first()
         # Assert
-        self.assertEqual(llist.head, None)
-        
+        self.assertEqual(self.llist.head, None)
+       
+    def test_delete_first_2(self):
         # Another case with 2 nodes
-        first_node = Node(item="First Node")
-        second_node = Node(item="Second Node")
-        first_node.next = second_node
-        llist = LinkedList(head=first_node)
-        llist.delete_first()
-        self.assertEqual(llist.head, second_node)
+        self.first_node.next = self.second_node
+        self.llist.delete_first()
+        self.assertEqual(self.llist.head, self.second_node)
+    
+    def test_insert_at(self):
+        self.first_node.next = self.second_node
+        new_node = Node(item="New Node")
+
+        # Act
+        self.llist.insert_at(1, new_node)
+
+        # Assert
+        self.assertEqual(self.llist.get_at(1), new_node)
+
+    def test_get_at(self):
+        self.first_node.next = self.second_node
+        self.assertEqual(self.llist.get_at(0), self.first_node)
+        self.assertEqual(self.llist.get_at(1), self.second_node)
+    
+    def test_set_at(self):
+        self.llist.set_at(0, "Changed First Node")
+        self.assertEqual(self.first_node.item, "Changed First Node")
 
 if __name__ == "__main__":
     unittest.main()
