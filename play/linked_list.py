29fca956dd71dc2ecc9ba17cb114b22a12ccf0dc
AmrAboshaisha <90650510+AmrAboshaisha@users.noreply.github.com>
2022-10-13 11:09:57 +0100

    Added insert_at, delete_at

--- play/linked_list.py
+++ play/linked_list.py
@@ -3,29 +3,34 @@ from linked_list_node import LinkedListNode as Node
 class LinkedList:
     def __init__(self, head=None):
         self.head = head
+        self.size = 0
+
+    def __len__(self):
+        return self.size
 
     def get_at(self, i):
         current_node = self.head
-        next_node = current_node.next
+        next_node = None 
         
-        if current_node == None: return None
-        else:
-            idx = 0
-            while idx != i:
-                idx += 1
-                current_node = next_node
-                next_node = next_node.next
-            return current_node
+        idx = 0
+        while idx != i:
+            next_node = current_node.next
+            current_node = next_node
+            idx += 1
+
+        return current_node
 
     def set_at(self, i, x):
         self.get_at(i).item = x
 
     def insert_first(self, x):
-        if self.head == None: self.head = x
-        else:
-            first_node = self.head
+        if self.head == None: 
             self.head = x
-            x.next = first_node
+        else:
+            x.next = self.head
+            self.head = x
+        
+        self.size +=1
 
     def delete_first(self):
         if self.head == None: pass
@@ -34,17 +39,34 @@ class LinkedList:
             second_node = first_node.next
             self.head = second_node
             return first_node
-
+        self.size -=1
+        
     def insert_at(self, i, x):
-        node_to_replace = self.get_at(i)
-        if node_to_replace == None:
+        if i == 0:
+            first_node = self.head
             self.head = x
-        else:
-            if i == 0:
-                self.head = x
-                x.next = node_to_replace
-            else:
-                previous_node = self.get_at(i-1)
-                previous_node.next = x
-                x.next = node_to_replace
+            x.next = first_node
 
+        else:
+            current_node = self.get_at(i)
+            previous_node = self.get_at(i-1)
+            x.next = current_node
+            previous_node.next = x
+
+        self.size +=1
+
+    def delete_at(self, i):
+        if i == 0:
+            current_node = self.get_at(i)
+            next_node = current_node.next
+            current_node.next = None
+            self.head = next_node
+        else:
+            previous_node = self.get_at(i-1)
+            current_node = previous_node.next
+            next_node = current_node.next
+
+            previous_node.next = next_node
+            current_node.next = None
+ 
+        self.size -=1
--- play/test_linked_list.py
+++ play/test_linked_list.py
@@ -8,6 +8,20 @@ class LinkedListTest(unittest.TestCase):
         self.first_node = Node(item="First Node")
         self.second_node = Node(item="Second Node")
         self.llist = LinkedList(head=self.first_node)
+    
+    def test_len(self):
+        # Case1: one node
+        llist = LinkedList()
+        llist.insert_at(0, self.first_node)
+        self.assertEqual(len(llist), 1)
+        # Case2: 2 nodes
+        llist = LinkedList()
+        llist.insert_first(self.first_node)
+        llist.insert_at(1, self.second_node)
+        self.assertEqual(len(llist), 2)
+        # Case3: Empty list
+        llist = LinkedList()
+        self.assertEqual(len(llist), 0)
 
     def test_non_empty_linked_list(self):
         self.assertEqual(self.llist.head.item, "First Node")
@@ -43,12 +57,34 @@ class LinkedListTest(unittest.TestCase):
 
     def test_get_at(self):
         self.first_node.next = self.second_node
+        third_node = Node(item="Third Node")
+        self.llist.insert_at(2, third_node)
+        #self.second_node.next = third_node
         self.assertEqual(self.llist.get_at(0), self.first_node)
         self.assertEqual(self.llist.get_at(1), self.second_node)
+        self.assertEqual(self.llist.get_at(2), third_node)
     
     def test_set_at(self):
         self.llist.set_at(0, "Changed First Node")
         self.assertEqual(self.first_node.item, "Changed First Node")
+
+    def test_delete_at(self):
+        #Case 1: delete from 0th node
+        self.llist.insert_at(1, self.second_node)
+        self.llist.delete_at(0)
+        self.assertEqual(self.llist.head.item, "Second Node")
+        # Case 2: delete from last node
+        llist = LinkedList(head=self.first_node)
+        self.first_node.next = self.second_node
+        llist.delete_at(1)
+        self.assertEqual(llist.head.next, None)
+        # Case 3: delete from middle node
+        llist = LinkedList(head=self.first_node)
+        self.first_node.next = self.second_node
+        third_node = Node(item="Third Node")
+        self.second_node.next = third_node
+        llist.delete_at(1)
+        self.assertEqual(llist.get_at(1), third_node)
 
 if __name__ == "__main__":
     unittest.main()
